e5c9737ea2ac226eabba67e1ccd7add07a8cc536
Mayur Darade <mayurdarade99@gmail.com>
2022-02-21 17:13:17 +0530

    update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,18 @@
-// import React, { useState } from 'react';
-
 const Form = ({ inputText, setInputText, todos, setTodos, status, setStatus, filteredTodos, setFilteredTodos}) => {
     //Javascript function to create a todo list object
     const inputTextHandler = (e) => {
-        setInputText(e.target.value);
+        setInputText(e.target.value); 
     };
 
     const submitTodoHandler = (e) => {
         e.preventDefault();
-        setTodos([ 
-            ...todos, 
-            { id: Math.random()*10, text: inputText, completed: false } 
-        ]);
-        setInputText(""); 
+        if (inputText.length > 0) {
+            setTodos([ 
+                ...todos, 
+                { id: Math.random()*10, text: inputText, completed: false } 
+            ]);
+            setInputText("");
+        } 
     }
 
     const statusHandler = (e) => {
@@ -27,7 +27,7 @@ const Form = ({ inputText, setInputText, todos, setTodos, status, setStatus, fil
             type="text" 
             className="form-control add-task" 
             placeholder="New Task..." />
-            <button onClick={submitTodoHandler} className="btn btn-primary">+</button>        
+            <button onClick={submitTodoHandler} className="btn btn-primary" id="addBtn">+</button>        
         </form>
         <ul className="nav nav-pills todo-nav">
             <li role="presentation" className="nav-item all-task active"><button onClick={statusHandler} className="nav-link" value="All">All</button></li>
